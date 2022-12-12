1575ea443d1732ae8f89937101aaee0b8fe14e56
Harry Twigg <harrytwigg111@gmail.com>
2022-12-12 15:23:30 +0000

    Include redis server

--- build-agent-linux.sh
+++ build-agent-linux.sh
@@ -1,34 +1,17 @@
-## Remove build directory if it exists
-#if [ -d build-agent-linux ]; then
-#    rm -rf build-agent-linux
-#fi
-#
-## Create build directory
-#mkdir build-agent-linux
-#cd build-agent-linux
-#
-## Clone redis source code into build-agent-linux directory from github
-#git clone https://github.com/redis/redis.git redis
-#
-## Change directory to redis source code
-#cd redis
-#
-## Build redis
-#make
-#cd ..
-#
-## Copy redis-server binary to build-agent-linux directory
-#cp redis/src/redis-server redis-server
-#
-## Remove redis source code
-#rm -rf redis
-#
-#cd ..
+# Remove build directory if it exists
+if [ -d build-agent-linux ]; then
+    rm -rf build-agent-linux
+fi
+
+# Create build directory
+mkdir build-agent-linux
+cd build-agent-linux
 
 # Build agent
 GOOS=linux GOARCH=amd64 go build -o build-agent-linux/apiteam-agent
 
 # Copy files to build directory
+cp targets/linux/redis-server build-agent-linux/redis-server
 cp targets/linux/apiteam-agent.desktop build-agent-linux/apiteam-agent.desktop
 cp targets/linux/snapcraft.yaml build-agent-linux/snapcraft.yaml
 cp targets/linux/run.sh build-agent-linux/run.sh
